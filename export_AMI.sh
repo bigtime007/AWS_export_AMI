2ceef59499a342e10e257866d9edeed65acdfe2e
Kevin <62586828+kcash333@users.noreply.github.com>
2021-07-09 21:37:18 -0500

    Update export_AMI.sh
    added s3 questions

--- export_AMI.sh
+++ export_AMI.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
-echo "Bucket Name"
-read BUCKET_NAME
-aws s3 mb $BUCKET_NAME
+echo "Do You Need a S3 Bucket to Export the AMI (y/n):"
+read a
+typeset -l n
+if [[ $a = y ]]
+then
+echo "Name S3 Bucket"
+read MB_NAME
+aws s3 mb s3://$MB_NAME
+exit
+else
+echo "No Bucket Added"
+fi
+echo "Add These Settings to Your export S3 Bucket"
+echo "Edit access control list (ACL) add access to other AWS accounts"
+echo "Grantee: c4d8eabf8db69dbe46bfe0e517100c554f01200b104d59cd408e777ba442a322"
+echo "Objects: List, Write | Bucket: ACL Read, Write"
+echo "Enter (y) when done. Any other key will abort the script"
+read b
+typeset -l n 
+if [[ $b = y ]]
+then
+echo "All done!"
+fi
 aws ec2 describe-instances
 echo "Input Instace ID Below, format i-xyx1234567890"
 read EC2_ID
